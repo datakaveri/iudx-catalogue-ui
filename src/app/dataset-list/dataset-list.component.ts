d9e70432014ec725f1526bda5f184687ffe78ba0
rnaaz <rohinanaaz@gmail.com>
2020-07-21 14:48:48 +0530

    Added content to search-results, Modified the css

--- src/app/dataset-list/dataset-list.component.ts
+++ src/app/dataset-list/dataset-list.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatasetListComponent implements OnInit {
   constructor() {}
-
+  providers = ['Provider 1', 'Provider 2', 'Provider 3', 'Provider 4'];
   ngOnInit(): void {}
+  getResourceGroups(value) {
+    console.log(value);
+    // Api call here to search and get the results.
+  }
+  getDataForProviders(event, option) {
+    // console.log(event.target.value, option);
+    console.log(event);
+    if (event.target.checked == true) {
+      console.log('Api will be called');
+      console.log(event.target.value, option);
+    }
+  }
 }
--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
-
-  constructor() { }
-
+  providers = ['Provider 1', 'Provider 2', 'Provider 3', 'Provider 4'];
+  listMapBtm = false;
+  constructor(private router: Router) {}
   ngOnInit(): void {
+    this.getRouter();
   }
-
+  getResourceGroups(value) {
+    console.log(value);
+    // Api call here to search and get the results.
+  }
+  getRouter() {
+    console.log(this.router.url);
+    if (this.router.url == '/saerch/items') {
+      this.listMapBtm = true;
+    }
+  }
 }
