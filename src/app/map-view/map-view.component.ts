75d0155c0dd9517b9b5cf14be56663e4a7959233
Yash Patki <yash.patki1@gmail.com>
2020-07-21 02:08:46 -0700

    Added map

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,15 +1,75 @@
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { Component, OnInit } from '@angular/core';
-
+import {
+  latLng,
+  tileLayer,
+  FeatureGroup,
+  DrawEvents,
+  icon,
+  Map,
+  Control,
+  map,
+  circle,
+  polygon,
+  layerGroup,
+  featureGroup,
+} from 'leaflet';
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
-  styleUrls: ['./map-view.component.scss']
+  styleUrls: ['./map-view.component.scss'],
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent {
+  showContainer: boolean = false;
+  drawnItems: FeatureGroup = featureGroup();
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Open Street Map',
+      }),
+    ],
+    zoom: 13,
+    center: latLng({ lat: 12.9716, lng: 77.5946 }),
+  };
 
-  constructor() { }
+  drawOptions = {
+    position: 'topright',
+    draw: {
+      marker: {
+        icon: icon({
+          iconSize: [25, 41],
+          iconAnchor: [13, 41],
+          iconUrl: './assets/marker.png',
+          iconRetinaUrl: '680f69f3c2e6b90c1812a813edf67fd7.png',
+          shadowUrl: 'a0c6cc1401c107b501efee6477816891.png',
+        }),
+      },
+    },
+    edit: {
+      featureGroup: this.drawnItems,
+    },
+  };
 
-  ngOnInit(): void {
+  drawLocal: any = {
+    draw: {
+      toolbar: {
+        buttons: {
+          polygon: 'Draw an awesome polygon!',
+        },
+      },
+    },
+  };
+
+  public onDrawCreated(e: any) {
+    // tslint:disable-next-line:no-console
+    console.log('Draw Created Event!');
+
+    const layer = (e as DrawEvents.Created).layer;
+    this.drawnItems.addLayer(layer);
   }
 
+  public onDrawStart(e: any) {
+    // tslint:disable-next-line:no-console
+    console.log('Draw Started Event!');
+  }
 }
