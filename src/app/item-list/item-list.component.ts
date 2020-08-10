60b9d02b642be7e052517e4dac05371469700312
rnaaz <rohinanaaz@gmail.com>
2020-08-10 22:43:39 +0530

    Implemented functionality for API in items page

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InterceptorService } from '../interceptor.service';
 import { ActivatedRoute } from '@angular/router';
+import { ConstantsService } from '../constants.service';
 
 @Component({
   selector: 'app-item-list',
@@ -10,71 +11,42 @@ import { ActivatedRoute } from '@angular/router';
 export class ItemListComponent implements OnInit {
   val: any;
   body: {};
-  resource_items: {};
-
-  // resource_items = [
-  //   {
-  //     name: 'sensorA',
-  //     description: 'Description of this resource group',
-  //     tags: 'sensor, sensing, resource, battery operated',
-  //     status: 'active',
-  //     resource_group: 'Description of this resource group',
-  //     provider: 'Provider Name',
-  //     control_level: 'level_1',
-  //     created_at: '2019-02-20T10:30:06.093121',
-  //     modified_at: '2019-02-20T10:30:06.093121',
-  //     location: {
-  //       type: 'Place',
-  //       address: 'IISc, Bangalore-560092, India',
-  //       geometry: {
-  //         type: 'Point',
-  //         coordinates: [75.92, 14.5],
-  //       },
-  //     },
-  //   },
-  //   {
-  //     name: 'sensorA',
-  //     description: 'Description of this resource group',
-  //     tags: 'sensor, sensing, resource, battery operated',
-  //     status: 'active',
-  //     resource_group: 'Description of this resource group',
-  //     provider: 'Provider Name',
-  //     control_level: 'level_1',
-  //     created_at: '2019-02-20T10:30:06.093121',
-  //     modified_at: '2019-02-20T10:30:06.093121',
-  //     location: {
-  //       type: 'Place',
-  //       address: 'IISc, Bangalore-560092, India',
-  //       geometry: {
-  //         type: 'Point',
-  //         coordinates: [75.92, 14.5],
-  //       },
-  //     },
-  //   },
-  // ];
+  items: any;
+  search_text: string;
+  pages: number;
+  searchQuery: {};
 
   constructor(
     private httpInterceptor: InterceptorService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private constantService: ConstantsService
   ) {
     this.body = {
       resource_groups: [],
       page: 0,
     };
+    this.search_text = '';
+    this.pages = 0;
+    this.searchQuery = {
+      search_text: '',
+      search_params: {
+        tags: [],
+        providers: [],
+        page: 0,
+        resource_groups: [],
+      },
+    };
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      console.log(params.id);
-      this.val = params.id;
-    });
-    this.getItems(this.val);
+    this.getItems();
   }
-  getItems(_id) {
+  getItems() {
+    this.body = this.constantService.get_search_query();
+    console.log(this.body);
     this.httpInterceptor.post_api('customer/items', this.body).then((res) => {
       console.log(res);
-      this.resource_items = res;
-      console.log(this.resource_items[0]);
+      this.items = res;
     });
   }
 }
