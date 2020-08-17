5a1f56ba2fc202fbae8f36b80b9575662fd1ab4c
Yash Patki <yash.patki1@gmail.com>
2020-08-17 05:09:22 -0700

    Editing for shapes in map

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -238,9 +238,10 @@ export class MapViewComponent {
     if (type === 'circle') {
       var center_point = e.layer._latlng;
       var radius = Math.ceil(e.layer._mRadius);
-      // console.log(center_point);
-      // console.log(radius);
+      console.log('Center1:' + center_point);
+      console.log('Radius1:' + radius);
       //Api call for getting items for that area
+
       this.httpInterceptor
         .get_api_test_map(
           `https://139.59.31.45:8443/iudx/cat/v1/search?geoproperty=location&georel=intersects&maxDistance=${radius}&geometry=Point&coordinates=[ ${center_point['lng']},${center_point['lat']}]`
@@ -339,8 +340,102 @@ export class MapViewComponent {
     this.markersLayer.clearLayers();
   }
   onDrawEdited(e: any) {
+    this.markerClusterGroup.clearLayers();
+    // this.markersLayer.clearLayers();
     console.log('Edited');
+    this.drawnItems.eachLayer((layer) => {
+      var layers = e.layers;
+      console.log(layers);
+      if (layer instanceof L.Circle) {
+        var center_point = layer.getLatLng();
+        console.log(center_point['lat']);
+        console.log(center_point['lng']);
+        var radius = Math.ceil(layer.getRadius());
+        console.log('Radius2:' + radius);
+        this.httpInterceptor
+          .get_api_test_map(
+            `https://139.59.31.45:8443/iudx/cat/v1/search?geoproperty=location&georel=intersects&maxDistance=${radius}&geometry=Point&coordinates=[${center_point['lng']},${center_point['lat']}]`
+          )
+          .then((res) => {
+            // console.log(res);
+            this.data = res;
+            for (var i = this.data.results.length - 1; i >= 0; i--) {
+              if (this.data.results[i].hasOwnProperty('location')) {
+                // myLayer.addData(data[i]['geoJsonLocation']);
+                this.plotGeoJSONs(
+                  this.data.results[i]['location']['geometry']['type'],
+                  this.data.results[i]['id'],
+                  this.data.results[i],
+                  this.data.results[i]['resourceGroup'],
+                  this.data.results[i]['provider']
+                );
+              } else if (
+                this.data.results[i].hasOwnProperty('coverageRegion')
+              ) {
+                // myLayer.addData(data[i]['geoJsonLocation']);
+                ////console.log("1")
+                this.plotGeoJSONs(
+                  res[i]['coverageRegion']['geometry'],
+                  this.data.results[i]['id'],
+                  this.data.results[i],
+                  this.data.results[i]['resourceGroup'],
+                  this.data.results[i]['provider']
+                );
+                ////console.log("2")
+              }
+            }
+          });
+      } else if (
+        layer instanceof L.Polygon &&
+        !(layer instanceof L.Rectangle)
+      ) {
+        // this.markersLayer.clearLayers();
+        console.log('editing poly...');
+
+        var polyPoints = [];
+        var _obj = Object.keys(layers._layers)[0];
+        var points = layers._layers[_obj]['_latlngs'][0];
+        for (var i = 0; i < points.length - 1; i += 1) {
+          var coordinates = [points[i]['lng'] + ',' + points[i]['lat']];
+          // console.log(coordinates);
+          polyPoints.push('[' + coordinates + ']');
+
+          polyPoints.join(',]');
+        }
+        console.log(polyPoints);
+        this.httpInterceptor
+          .get_api_test_map(
+            `https://139.59.31.45:8443/iudx/cat/v1/search?geoproperty=location&georel=within&geometry=Polygon&coordinates=[[${polyPoints},${polyPoints[0]}]]`
+          )
+          .then((res) => {
+            // console.log(res);
+            this.data = res;
+            for (var i = this.data.results.length - 1; i >= 0; i--) {
+              if (this.data.results[i].hasOwnProperty('location')) {
+                this.plotGeoJSONs(
+                  this.data.results[i]['location']['geometry']['type'],
+                  this.data.results[i]['id'],
+                  this.data.results[i],
+                  this.data.results[i]['resourceGroup'],
+                  this.data.results[i]['provider']
+                );
+              } else if (
+                this.data.results[i].hasOwnProperty('coverageRegion')
+              ) {
+                this.plotGeoJSONs(
+                  res[i]['coverageRegion']['geometry'],
+                  this.data.results[i]['id'],
+                  this.data.results[i],
+                  this.data.results[i]['resourceGroup'],
+                  this.data.results[i]['provider']
+                );
+              }
+            }
+          });
+      }
+    });
   }
+
   // onDrawStart(e: any) {
   //   console.log('Draw Started Event!');
   //   // this.markerClusterGroup.clearLayers();
