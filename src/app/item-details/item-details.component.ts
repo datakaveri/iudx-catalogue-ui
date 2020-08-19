adce71c654c925ad2457e63b1b2387f54e0b310b
unknown <aakashsri.dev@gmail.com>
2020-08-19 19:31:09 +0530

    showing data on items details pages

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -8,10 +8,14 @@ import { ConstantsService } from '../constants.service';
 })
 export class ItemDetailsComponent implements OnInit {
   resource: any;
+  texts: any;
   constructor(
     private constant: ConstantsService
   ) {
     this.resource = this.constant.get_resource_details();
+    console.log(this.resource);
+    this.texts = this.constant.get_nomenclatures();
+    
   }
 
   ngOnInit(): void {
