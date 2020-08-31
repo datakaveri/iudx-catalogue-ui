813982a8abd4e63bf1e026f3883103e415633cfc
unknown <aakashsri.dev@gmail.com>
2020-08-31 17:28:32 +0530

    moved latestData to it's own component

--- src/app/item-res-items/item-res-items.component.ts
+++ src/app/item-res-items/item-res-items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ConstantsService } from '../constants.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-res-items',
@@ -11,103 +12,29 @@ export class ItemResItemsComponent implements OnInit {
   texts: any;
   resourceAuthControlLevel: string;
   showPopup: boolean;
-  lattesData: any;
   overlay: any;
 
   constructor(
-    private constant: ConstantsService
+    private constant: ConstantsService,
+    private router: Router
   ) {
     this.resource = this.constant.get_resource_details();
     this.resourceAuthControlLevel = this.resource.resource_group.resourceAuthControlLevel;
     this.texts = this.constant.get_nomenclatures();
     this.overlay = false;
     this.showPopup = false;
-    this.lattesData = [
-      {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }, {
-        'HOTSPOT_ID': 71,
-        'USER_COUNT': 0,
-        'NAME': 'In front of kalyan nagar steel',
-        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
-        'LOCATION_STATUS': 'ON',
-        'ACCESS_POINT_COUNT': 1
-      }
-    ]
   }
 
   ngOnInit(): void {
   }
 
-  showSampleData(){
-    this.showPopup = true;
-    this.overlay = true;
+  showLatestData(){
+    this.router.navigate(['/search/dataset/items/latest-data']);
   }
 
-  hideSampleData(){
-    this.showPopup = false;
-    this.overlay = false;
-  }
+  // hideSampleData(){
+  //   this.showPopup = false;
+  //   this.overlay = false;
+  // }
 
 }
--- src/app/latest-data/latest-data.component.ts
+++ src/app/latest-data/latest-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-latest-data',
@@ -7,9 +8,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LatestDataComponent implements OnInit {
 
-  constructor() { }
+  latestData: any;
+  overlay: boolean;
+  showLatest:boolean;
+  lattesData:any;
+
+  constructor(private router: Router) {
+
+    this.showLatest = true;
+    this.overlay = true;
+
+    this.lattesData = [
+      {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }, {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }
+    ]
+   }
 
   ngOnInit(): void {
   }
 
+  hideLatestData(){
+    this.router.navigate(['../../search/dataset/items']);
+    this.showLatest = false;
+    this.overlay = false;
+  }
 }
