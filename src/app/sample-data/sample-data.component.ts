259edb8e7ca124ad7512de0530f186defa689f2e
unknown <aakashsri.dev@gmail.com>
2020-08-31 17:01:24 +0530

    moved sampleData to its own component

--- src/app/sample-data/sample-data.component.ts
+++ src/app/sample-data/sample-data.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sample-data',
   templateUrl: './sample-data.component.html',
   styleUrls: ['./sample-data.component.scss']
 })
 export class SampleDataComponent implements OnInit {
+  sampleData: any;
+  overlay: boolean;
+  showSample:boolean;
+  
+  constructor(private router: Router) {
 
-  constructor() { }
+    this.showSample = true;
+    this.overlay = true;
+    this.sampleData = [
+      {
+        'HOTSPOT_ID': 71,
+        'USER_COUNT': 0,
+        'NAME': 'In front of kalyan nagar steel',
+        'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
+        'LOCATION_STATUS': 'ON',
+        'ACCESS_POINT_COUNT': 1
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
 
+  hideSampleData(){
+    this.router.navigate(['../search/datasets/']);
+    this.showSample = false;
+    this.overlay = false;
+  }
 }
