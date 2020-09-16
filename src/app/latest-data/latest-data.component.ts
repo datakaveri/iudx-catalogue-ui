055c7a3a52cd4dbc70d1f540f6856d491e80ba5c
rnaaz <rohinanaaz@gmail.com>
2020-09-16 23:36:21 +0530

    formatted the json view for latestData

--- src/app/latest-data/latest-data.component.ts
+++ src/app/latest-data/latest-data.component.ts
@@ -12,6 +12,7 @@ export class LatestDataComponent implements OnInit {
   latestData: any;
   overlay: boolean;
   showLatest:boolean;
+  res: string;
 
   constructor(
     private router: Router,
@@ -21,7 +22,10 @@ export class LatestDataComponent implements OnInit {
     this.showLatest = true;
     this.overlay = true;
 
-    this.latestData = [
+     this.latestData = 
+
+    //  [{"@context":"https://voc.iudx.org.in/","type":["iudx:ResourceGroup"],"id":"providerID/example.com/senosrs","description":"Description of this resource group","name":"sensors","tags":"sensor, sensing, resource, battery operated","itemStatus":"ACTIVE","provider":"providerID","resourceServer":"resourceProviderID/exapmle.com","resourceAuthControlLevel":"INDIVIDUAL","resourceType":"messageStream","authServerInfo":{"type":["AuthServerInfoValue"],"authServerURL":"https://myauth.server.org/","authType":"iudx-auth"},"accessObjectInfo":{"type":["AccessObjectInfoValue"],"accessObject":"https://example.com/sensorsApis.json","additionalInfoURL":"https://example.com/sensorsApis","accessObjectType":"openAPI"},"iudxResourceAPIs":["attribute","temporal"],"itemCreatedAt":"2019-02-20T10:30:06.093121","location":{"type":"Place","address":"Bangalore"}}]
+     [
       {
         'HOTSPOT_ID': 71,
         'USER_COUNT': 0,
@@ -29,7 +33,8 @@ export class LatestDataComponent implements OnInit {
         'LASTUPDATETIME': '2020-03-10T13:33:21+05:30',
         'LOCATION_STATUS': 'ON',
         'ACCESS_POINT_COUNT': 1
-      }, {
+      }, 
+      {
         'HOTSPOT_ID': 71,
         'USER_COUNT': 0,
         'NAME': 'In front of kalyan nagar steel',
